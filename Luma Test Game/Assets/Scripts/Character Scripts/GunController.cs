ee901a7fb48df702ed3de01142d0b5f6951a5b06
Allan <woodwormo@gmail.com>
2019-12-11 19:44:01 +0200

    Initial commit

---
+++ Luma Test Game/Assets/Scripts/Character Scripts/GunController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    //rate of fire to be increased as per level and random drops
+    [SerializeField]
+    [Range (0.5f, 1.5f)]
+    float fireRange = 1;
+
+
+    // damage to be increased as per level and random drops
+    [SerializeField]
+    [Range(1f, 10f)]
+    int damage = 1;
+
+
+
+
+
+}
